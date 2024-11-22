b769b4b6d51c2299a6749120763a248ffee5183c
AlexandreCSDV <alexandre.sousa@dellavolpe.com.br>
2024-11-22 11:13:21 -0300

    Update MapaDeCarregamento.py

--- MapaDeCarregamento.py
+++ MapaDeCarregamento.py
@@ -21,14 +21,16 @@ def dataframe_to_pdf(dataframe_dict, buffer):
     subtitle_style = styles["Heading2"]
     centered_subtitle_style = ParagraphStyle(name="CenteredSubtitle", parent=subtitle_style, alignment=1)
     centered_title_style = ParagraphStyle(name="CenteredTitle", parent=title_style, alignment=1)
-
+    
     zona_entrega_mais_comum = df['Zona Entrega'].mode()[0]
     total_nfs = df['Nota'].nunique()
     peso_por_status = df.groupby('Status')['KG'].sum().round(2).to_dict()
-    status_peso_text = ' \\ '.join([f"{status}: {peso} KG" for status, (peso) in peso_por_status.items()])
+    status_peso_text = ' \\\\ '.join([f"{status}: {peso} KG" for status, (peso) in peso_por_status.items()])
+    
     title_text_line1 = f"Relatório de Carregamento - Zona de Entrega: {zona_entrega_mais_comum}"
     title_text_line2 = f"Peso Total: {subtotal_total} KG - NFs Totais: {total_nfs}"
     title_text_line3 = f"{status_peso_text}"
+    
     header_elements = [
         Paragraph(title_text_line1, centered_title_style),
         Spacer(1, -0.3 * inch),
@@ -37,14 +39,16 @@ def dataframe_to_pdf(dataframe_dict, buffer):
         Paragraph(title_text_line3, centered_title_style),
         Spacer(1, 0.00005 * inch)
     ]
+    
     header_table = Table([[header_elements]], colWidths=[landscape(A4)[0]])
     header_table.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, -1), colors.white)]))
-
+    
     subtitle_line1 = f"Notas Dentro do Prazo"
     subtitle_elements = [
         Paragraph(subtitle_line1,title_style),
         Spacer(1, -0.3 * inch)
     ]
+    
     subheader_table = Table([[subtitle_elements]], colWidths=[landscape(A4)[0]])
     
     first_pdf = "primeiro"
@@ -53,8 +57,6 @@ def dataframe_to_pdf(dataframe_dict, buffer):
         if first_pdf == "primeiro":
             elements.append(header_table)
             first_pdf = "segundo"
-        #elif first_pdf == "segundo":
-         #   elementssubheader_table
         
         subtotal_peso = round(dataframe['KG'].sum(), 2)
         subtotal_notas = dataframe['Nota'].nunique()
@@ -90,9 +92,10 @@ def dataframe_to_pdf(dataframe_dict, buffer):
     
     pdf.build(elements)
 
-# Função para ordenar DataFrame por almoxarifado
-def teste(df):
-    ordem_regiao_wms = ['Quimico','Cantilever', 'Praça Externa 1', 'Praça Externa 2', 'Praça - Rua 00 - Posição 01',
+# Função para ordenar DataFrame por almoxarifado ou região WMS
+def ordenar_dataframe(df):
+    if 'Região WMS' in df.columns:
+        ordem_regiao_wms = ['Quimico','Cantilever', 'Praça Externa 1', 'Praça Externa 2', 'Praça - Rua 00 - Posição 01',
                         'Praça - Rua 01 - Posição 05', 'Praça - Rua 01 - Posição 06','Praça - Rua 01 - Posição 07',
                         'Praça - Rua 01 - Posição 08','Praça - Rua 01 - Posição 09','Praça - Rua 02 - Posição 01',
                         'Praça - Rua 02 - Posição 02','Praça - Rua 02 - Posição 03','Praça - Rua 02 - Posição 04',
@@ -140,33 +143,34 @@ def teste(df):
                         'Porta Palete C', 'Porta Palete D', 'Porta Palete E', 'Porta Palete F', 'Porta Palete G',
                         'Porta Palete H', 'Porta Palete I', 'Porta Palete J', 'Porta Palete K', 'Praça de Consolidação',
                         'Rua 08', 'Rua 12', 'Rua 13', 'Rua 14', 'Rua 15', 'Rua 16', 'Stage', 'Sem Posição']
+        
+        dataframes_por_almoxarifado_regiao_wms = dict(tuple(df.groupby('Região WMS')))
+        
+        dataframes_ordenados_regiao_wms = OrderedDict({
+            regiao: dataframes_por_almoxarifado_regiao_wms[regiao]
+            for regiao in ordem_regiao_wms if regiao in dataframes_por_almoxarifado_regiao_wms
+        })
+        
+        dataframes_ordenados_regiao_wms.update({
+            regiao: df for regiao, df in dataframes_por_almoxarifado_regiao_wms.items()
+            if regiao not in ordem_regiao_wms
+        })
+        
+        return dataframes_ordenados_regiao_wms
     
-    dataframes_por_almoxarifado = dict(tuple(df.groupby('Região WMS')))
-    
-    dataframes_ordenados = OrderedDict({
-        regiao: dataframes_por_almoxarifado[regiao]
-        for regiao in ordem_regiao_wms if regiao in dataframes_por_almoxarifado
-    })
-    
-    dataframes_ordenados.update({
-        regiao: df for regiao, df in dataframes_por_almoxarifado.items()
-        if regiao not in ordem_regiao_wms
-    })
-    
-    return dataframes_ordenados
+    else:
+        return dict(tuple(df.groupby('Almoxarifado')))
 
 # Interface do Streamlit
 st.title("Gerador de PDF de Carregamento")
-
 uploaded_file = st.file_uploader("Escolha um arquivo Excel", type="xlsx")
-
 if uploaded_file is not None:
     df = pd.read_excel(uploaded_file)
-
+    
     # Realiza as manipulações no DataFrame
     df = df.iloc[:-2]
-    df = df[['Chegada', 'Nota Numero', 'Etiqueta Unica', 'Chave Conhecimento', 'Peso Nota', 'Nota Volumes', 
-             'Tp. Solicitacao Coleta', 'Nº ONU', 'Razao Remetente', 'Almox. Destino', 'Mercadoria Descricao', 
+    df = df[['Chegada', 'Nota Numero', 'Etiqueta Unica', 'Chave Conhecimento', 'Peso Nota', 'Nota Volumes',
+             'Tp. Solicitacao Coleta', 'Nº ONU', 'Razao Remetente', 'Almox. Destino', 'Mercadoria Descricao',
              'Limite Entregar (Definitivo)', 'Endereco WMS', 'Data Limite Embarque', 'Zona Entrega', 'Região WMS']]
     df['Chave Conhecimento'] = df['Chave Conhecimento'].fillna('')
     df['Tp. Solicitacao Coleta'] = df['Tp. Solicitacao Coleta'].str.slice(0, 1)
@@ -181,11 +185,9 @@ if uploaded_file is not None:
     df['Data Limite Embarque'] = df['Data Limite Embarque'].dt.strftime('%d/%m/%y')
     df['Nota Numero'] = df['Nota Numero'].astype(int)
     df['Nota Volumes'] = df['Nota Volumes'].astype(int)
-    df['Etiqueta Unica'] = pd.to_numeric(df['Etiqueta Unica'], errors='coerce').fillna('')
-    df['Etiqueta Unica'] = df['Etiqueta Unica'].astype('string')
+    df['Etiqueta Unica'] = pd.to_numeric(df['Etiqueta Unica'], errors='coerce').fillna('').astype('string')
     df['Etiqueta Unica'] = df['Etiqueta Unica'].apply(lambda x: x[:-2] if x.endswith('.0') else x).replace('nan', '')
-    df['Nº ONU'] = pd.to_numeric(df['Nº ONU'], errors='coerce').fillna('')
-    df['Nº ONU'] = df['Nº ONU'].astype('string')
+    df['Nº ONU'] = pd.to_numeric(df['Nº ONU'], errors='coerce').fillna('').astype('string')
     df['Nº ONU'] = df['Nº ONU'].apply(lambda x: x[:-2] if x.endswith('.0') else x).replace('nan', '')
     df['Endereco WMS'] = df['Endereco WMS'].fillna('')
     df['Peso Nota'] = df['Peso Nota'].round(2)
@@ -213,7 +215,7 @@ if uploaded_file is not None:
     subtotal_total = round(df['KG'].sum(), 2)
     df.loc[:, 'Status'] = ''
     today = datetime.today().date()
-
+    
     for index, row in df.iterrows():
         try:
             data_embarque = pd.to_datetime(row['Lim. Embarque'], format='%d/%m/%y').date()
@@ -233,38 +235,37 @@ if uploaded_file is not None:
             df.at[index, 'Status'] = 'Dentro do Prazo'
     
     df['Lim. Embarque'] = df['Lim. Embarque'].dt.strftime('%d/%m/%Y')
-
+    
     # Processamento dos DataFrames e geração dos PDFs
     df_hoje = df.loc[df['Status'] == 'Dentro do Prazo']
     df_atrasado = df.loc[df['Status'] != 'Dentro do Prazo']
-
-    df_atrasado = teste(df_atrasado)
-    df_hoje = teste(df_hoje)
-
-# Buffer para armazenar os PDFs
+    df_atrasado = ordenar_dataframe(df_atrasado)
+    df_hoje = ordenar_dataframe(df_hoje)
+    
+    # Buffer para armazenar os PDFs
     buffer_atrasado = io.BytesIO()
     buffer_hoje = io.BytesIO()
-
-# Gerar os PDFs
+    
+    # Gerar os PDFs
     dataframe_to_pdf(df_atrasado, buffer_atrasado)
     dataframe_to_pdf(df_hoje, buffer_hoje)
-
-# Mesclar os PDFs
+    
+    # Mesclar os PDFs
     buffer_atrasado.seek(0)
     buffer_hoje.seek(0)
     merger = PdfMerger()
     merger.append(buffer_atrasado)
     merger.append(buffer_hoje)
-
-# Salvar o PDF final em um buffer
+    
+    # Salvar o PDF final em um buffer
     output_buffer = io.BytesIO()
     merger.write(output_buffer)
     merger.close()
-
-# Botão para download do PDF
+    
+    # Botão para download do PDF
     st.download_button(
-    label="Baixar PDF",
-    data=output_buffer.getvalue(),
-    file_name="MapaDeCarregamento.pdf",
-    mime="application/pdf"
-)
+        label="Baixar PDF",
+        data=output_buffer.getvalue(),
+        file_name="MapaDeCarregamento.pdf",
+        mime="application/pdf"
+    )
